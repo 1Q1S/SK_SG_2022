6a232aec3f8f8a4026127374188b4bb48480d352
1Q1S <dl2335913@naver.com>
2022-06-03 13:06:58 +0900

    20220603

--- todaymenu.py
+++ todaymenu.py
@@ -3,18 +3,36 @@ from selenium.webdriver.common.keys import Keys
 import time
 from datetime import datetime
 
+def fomating_date(date):
+    day = text[-4:]
+    text = text.removesuffix(day)
+    text=text.replace('년',',').replace('월',',').replace('일',',').replace(' ','')
+    text=text.split(',')
+
+    year=text[0]
+    month=text[1].zfill(2)
+    days=text[2].zfill(2)
+
+    return year, month, days, day
+
+
 url = 'http://massmail.sk.com/menu/print.aspx?rcode=no11'
-driver = webdriver.Chrome('.\chromedriver.exe')
-driver.get(url)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+browser = webdriver.Chrome(options=options)
+#driver = webdriver.Chrome('.\chromedriver.exe')
+browser.get(url)
 time.sleep(5)
 
-news_titles = driver.find_elements_by_css_selector("#form1 > div:nth-child(3) > table > tbody > tr:nth-child(1) > th")
+for i in range(1,8):
+    news_titles = browser.find_elements_by_css_selector("#form1 > div:nth-child(3) > table > tbody > tr:nth-child(%d) > th"%(i))
+    news_text = [i.text for i in news_titles]
+    time.sleep(1)
+    for text in news_text:
+        year, month, days,day = fomating_date(text)
+        
+        print(year+'년'+month+'월'+days+'일'+day)
 
-time.sleep(2)
-
-for i in news_titles:
-    a=i.text()
-    print(a)
 
 
 # now = datetime.now()
