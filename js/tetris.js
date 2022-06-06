3b1d45bcfd39ed9b783fbd32d48c23eb11250466
1Q1S <dl2335913@naver.com>
2022-06-06 20:30:53 +0900

    20220606

---
+++ js/tetris.js
@@ -0,0 +1,210 @@
+import BLOCKS from "./blocks.js";
+
+// DOM
+
+const playground = document.querySelector(".playground > ul");
+const gameText = document.querySelector(".game-text")
+const scoreDisplay = document.querySelector(".score")
+const resartbutton = document.querySelector(".game-text > button")
+//Setting
+
+const GAME_ROWS=20;
+const GAME_COLS=10;
+
+// variables
+
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempmovingItem;
+let movingitem={
+    type:"" ,
+    direction :0,
+    top: 0,
+    left: 3,
+};
+
+
+
+// functions
+init();
+
+function init(){
+    
+    tempmovingItem = {...movingitem};
+
+    for (let i =0; i <GAME_ROWS; i++){
+        prependNewline()
+        
+    }
+    generateNesbolck()
+}
+
+
+function prependNewline(){
+    const li=document.createElement("li");
+    const ul=document.createElement("ul");
+    for (let j=0; j<GAME_COLS; j++){
+        const matrix=document.createElement("li");
+        ul.prepend(matrix);
+    }
+    li.prepend(ul);
+    playground.prepend(li);
+}
+
+function renderBlocks(moveType=""){
+    const {type, direction, top, left} = tempmovingItem;
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving => {
+        moving.classList.remove(type,"moving");        
+    });
+
+    BLOCKS[type][direction].some(block=>{
+        const x = block[0]+left;
+        const y = block[1]+top;
+        
+
+        // 변수 xxx= 조건 ? 참일경우: 거짓일 경우
+        const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
+        console.log(target)
+        const isAvailable = checkEmpty(target);
+
+        if(isAvailable){
+            target.classList.add(type,"moving");
+        } else{
+            tempmovingItem={...movingitem};
+            if (moveType==="retry"){
+                clearInterval(downInterval);
+                showGameoverText();
+            }
+            setTimeout(()=>{
+                renderBlocks("retry");
+                if(moveType==="top"){
+                    seizeBlock();
+                }
+                
+            },0)
+            //renderBlocks();
+            return true;
+        }
+        });
+        
+        movingitem.left=left;
+        movingitem.top=top;
+        movingitem.direction=direction;
+        
+
+    
+   
+}
+function showGameoverText(){
+    gameText.style.display ="flex"
+
+}
+
+function seizeBlock(){
+
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving => {
+        moving.classList.remove("moving");
+        moving.classList.add("seized");
+    });
+    checkMatch()
+}
+
+function checkMatch(){
+    const childNodes = playground.childNodes;
+    childNodes.forEach(child=>{
+        let matched =true;
+        child.children[0].childNodes.forEach(li=>{
+            if(!li.classList.contains("seized")){
+                matched =false;
+            }
+        })
+        if(matched){
+            child.remove();
+            prependNewline();
+            score++;
+            scoreDisplay.innerText=score;
+
+        }
+    })
+
+    generateNesbolck();
+
+
+}
+
+function generateNesbolck(){
+
+    clearInterval(downInterval);
+    downInterval=setInterval(()=>{
+        moveBlock("top",1);
+    },duration);
+
+    const blockArray=Object.entries(BLOCKS);
+    const randomIndex=Math.floor(Math.random()*blockArray.length);
+    movingitem.type=blockArray[randomIndex][0];
+    movingitem.left=3;
+    movingitem.top=0;
+    movingitem.direction=0;
+    tempmovingItem={...movingitem};
+    renderBlocks();
+}
+
+function checkEmpty(target){
+    
+    if(!target || target.classList.contains("seized")) {
+        return false;
+    }
+    return true;
+}
+
+function moveBlock(moveType, amount){
+    tempmovingItem[moveType] += amount;
+    renderBlocks(moveType)
+}
+
+function changedirection(){
+    const direction = tempmovingItem.direction;
+    direction===3 ? tempmovingItem.direction =0 : tempmovingItem.direction+=1;
+
+    renderBlocks()
+}
+
+function dropBlock(){
+    clearInterval(downInterval);
+    downInterval=setInterval(()=>{
+        moveBlock("top",1)
+    },10)
+}
+
+// event handling
+document.addEventListener("keydown",e=>{
+    switch(e.keyCode){
+        case 39:
+            moveBlock("left",1);
+            break;
+        case 37:
+            moveBlock("left",-1);
+            break;
+        case 40:
+            moveBlock("top",1);
+            break;
+        case 38:
+            changedirection();
+            break;
+        case 32:
+            dropBlock();
+            break;
+        default:
+            break;
+    }
+    //console.log(e)
+})
+
+resartbutton.addEventListener("click", () => {
+    playground.innerHTML = "";
+    gameText.style.display="none"
+    init();
+})
--- todaymenu.py
+++ todaymenu.py
@@ -1,17 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys                        
 import time
+from time import strftime
 from datetime import datetime
 
-def fomating_date(date):
-    day = text[-4:]
-    text = text.removesuffix(day)
-    text=text.replace('년',',').replace('월',',').replace('일',',').replace(' ','')
-    text=text.split(',')
+def today_time():
+    import time
+    from time import strftime
 
-    year=text[0]
-    month=text[1].zfill(2)
-    days=text[2].zfill(2)
+    tm = time.localtime(time.time())
+    
+    date=strftime('%Y년 %m월 %d일', tm)
+    time=strftime('%H:%M:%S', tm)
+
+    return date, time
+
+def fomating_date(date):
+    day = date[-4:].replace(' ','')
+    date = date.replace(day,'')
+    date=date.replace('년',',').replace('월',',').replace('일',',').replace(' ','')
+    date=date.split(',')
+
+    year=date[0]
+    month=date[1].zfill(2)
+    days=date[2].zfill(2)
 
     return year, month, days, day
 
@@ -19,6 +31,7 @@ def fomating_date(date):
 url = 'http://massmail.sk.com/menu/print.aspx?rcode=no11'
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument('--headless')
 browser = webdriver.Chrome(options=options)
 #driver = webdriver.Chrome('.\chromedriver.exe')
 browser.get(url)
@@ -26,14 +39,84 @@ time.sleep(5)
 
 for i in range(1,8):
     news_titles = browser.find_elements_by_css_selector("#form1 > div:nth-child(3) > table > tbody > tr:nth-child(%d) > th"%(i))
+    menu=browser.find_elements_by_xpath('//*[@id="form1"]/div[3]/table/tbody/tr[%d]/td[1]/ul/li'%(i))
     news_text = [i.text for i in news_titles]
+    menu_text= [i.text for i in menu]
     time.sleep(1)
     for text in news_text:
-        year, month, days,day = fomating_date(text)
+        year, month, days,day = fomating_date(text)    
+        date = year+'년 '+month+'월 '+days+'일'
         
-        print(year+'년'+month+'월'+days+'일'+day)
+        to_date,to_time=today_time()    
+    
+        if to_date == date:
+            a=date
+            b=menu_text
+            
+#form1 > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(2)
 
-
-
+#form1 > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(2) > ul > li:nth-child(1)
+#form1 > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > ul
+#form1 > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > ul > li:nth-child(1)
+#form1 > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > ul > li:nth-child(1)
 # now = datetime.now()
 # now.strftime("%Y년 %m월 %d일")   
+# //*[@id="form1"]/div[3]/table/tbody/tr[2]/td[1]/ul
+# //*[@id="form1"]/div[3]/table/tbody/tr[1]/td[1]/ul
+
+from selenium.webdriver.common.keys import Keys                        
+from selenium import webdriver
+import time
+from time import strftime
+from datetime import datetime
+
+def today_time():
+    import time
+    from time import strftime
+
+    tm = time.localtime(time.time())
+    
+    date=strftime('%Y년 %m월 %d일', tm)
+    time=strftime('%H:%M:%S', tm)
+
+#     return date, time
+
+# def fomating_date(date):
+#     day = date[-4:].replace(' ','')
+#     date = date.replace(day,'')
+#     date=date.replace('년',',').replace('월',',').replace('일',',').replace(' ','')
+#     date=date.split(',')
+
+#     year=date[0]
+#     month=date[1].zfill(2)
+#     days=date[2].zfill(2)
+
+#     return year, month, days, day
+
+'''20220604'''
+# url = 'http://massmail.sk.com/menu/print.aspx?rcode=no11'
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach", True)
+# options.add_experimental_option("excludeSwitches", ["enable-logging"])
+# options.add_argument('--headless')
+# browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+# #driver = webdriver.Chrome('.\chromedriver.exe')
+# browser.get(url)
+# time.sleep(5)
+
+# for i in range(1,8):
+#     news_titles = browser.find_elements_by_css_selector("#form1 > div:nth-child(3) > table > tbody > tr:nth-child(%d) > th"%(i))
+#     menu=browser.find_elements_by_xpath('//*[@id="form1"]/div[3]/table/tbody/tr[%d]/td[1]/ul/li'%(i))
+#     news_text = [i.text for i in news_titles]
+#     menu_text= [i.text for i in menu]
+#     time.sleep(1)
+#     for text in news_text:
+#         year, month, days,day = fomating_date(text)    
+#         date = year+'년 '+month+'월 '+days+'일'
+        
+#         to_date,to_time=today_time()    
+    
+#         if to_date == date:
+#             print(date)
+#             print('오늘의 메뉴:',end='')
+#             print(menu_text)
